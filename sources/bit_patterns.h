7212f113c2f4a4156d7412d8aababf482820516e
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-04-05 09:48:18 +0200

    i

--- sources/bit_patterns.h
+++ sources/bit_patterns.h
@@ -1,10 +1,25 @@
-#ifndef TYPE_FORMATS_H
-# define TYPE_FORMATS_H
+#ifndef BIT_PATTERNS_H
+# define BIT_PATTERNS_H
 
 # ifdef A
 typedef union	u_double {
 	double	d;
+	char	ar[sizeof(double)];
 	struct	{
-			
-
+		unsigned long long	sign : 1;
+		unsigned long long	exponent : 11;
+		unsigned long long	significand : 52;
+	}		decomposition;
+}				t_u_d;
+# elif defined B
+typedef union	u_double {
+	double	d;
+	char	ar[sizeof(double)];
+	struct	{
+		unsigned long long	significand : 52;
+		unsigned long long	exponent : 11;
+		unsigned long long	sign : 1;
+	}		decomposition;
+}				t_u_d;
+# endif
 #endif
