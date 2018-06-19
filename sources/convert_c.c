0398b9e28047ea2e1c5d4c1000d881467b2a5a27
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-06-19 21:37:36 +0200

    i

--- sources/convert_c.c
+++ sources/convert_c.c
@@ -6,6 +6,24 @@ static void
 	chk->flags &= MINUS_FLAG;
 }
 
-void		convert_c(t_s_pct *p_chk)
+void		convert_c(t_s_pct *chk)
 {
+	wint_t	c;
+	size_t	lens[e_oi_sz];
+	char	ar[UTF8_MAX_CHARS];
+
+	lens[e_prefix] = 0;
+	if (chk->vaarg->type == e_wint_t)
+	{
+		if ((c = *(wint_t*)chk->vaarg->p_val) < 0)
+			lens[e_root] = 0;
+		else
+			lens[e_root] = true_utf8seq(c, ar);
+	}
+	else
+	{
+		*ar = *(unsigned char*);
+		lens[e_root] = 1;
+	}
+	output_padnbuffer(ar, lens, chk);
 }
--- sources/my_utf8_inner.h
+++
@@ -1,7 +0,0 @@
-#ifndef MY_UTF8_INNER_H
-# define MY_UTF8_INNER_H
-
-
-int		const g_utf8_max = UTF8_MAX_CHARS;
-
-#endif
