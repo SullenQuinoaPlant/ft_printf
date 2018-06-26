4a8156cd9a387dcfe45ff3ea30a61ed9fa5b7999
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-06-26 16:29:24 +0200

    i

---
+++ sources/convert_e_stuffers1.c
@@ -0,0 +1,46 @@
+#include "ft_printf_inner.h"
+
+void
+	ce_sign(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
+
+void
+	ce_power0(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
+
+void
+	ce_separator(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
+
+void
+	ce_lowpowers(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
+
+void
+	ce_excess_precision(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
---
+++ sources/convert_e_stuffers2.c
@@ -0,0 +1,19 @@
+#include "ft_printf_inner.h"
+
+void
+	ce_to_the_power(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
+
+void
+	ce_exponent(
+		int pos, void *s)
+{
+	t_s_ecs	* const stf = (t_s_ecs*)s;
+
+	(void)stf;
+}
