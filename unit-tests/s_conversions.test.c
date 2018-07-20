e5b733fc23fe10cd75b44a9866da6aeb86a3abe3
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-20 12:43:00 +0200

    i

--- unit-tests/s_conversions.test.c
+++ unit-tests/s_conversions.test.c
@@ -1,6 +1,8 @@
 #define HOW_MANY_TESTS 100
 #include "cmocka/my_overlay.h"
 
+#include "printf_diff.c"
+
 int	declare_tests_and_run(int all_of, char *these[])
 {
 	T(test_unicode1,
@@ -12,13 +14,17 @@ int	declare_tests_and_run(int all_of, char *these[])
 	)
 
 	T(test_unicode2,
-		wchar_t	vals[5] = {0xc8,  0x0a, 0xa9, 0x0a, 0};
+		wchar_t	*vals;
+
+		vals = ((wchar_t[5]){0xc8,  0x0a, 0xa9, 0x0a, 0});
 
 		printf_compare("s_test_unicode2.ref", "%.3ls", vals);
 	)
 
 	T(test_unicode3,
-		wchar_t	vals[5] = {0xc8,  0x0a, 0xa9, 0x0a, 0};
+		wchar_t	*vals;
+
+		vals = ((wchar_t*){0xc8,  0x0a, 0xa9, 0x0a, 0});
 
 		printf_compare("s_test_unicode2.ref", "%.4ls", vals);
 	)
