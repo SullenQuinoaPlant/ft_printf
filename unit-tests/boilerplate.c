ff4a76d06c7c178ec5f4385a756932ed911facc3
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-20 12:23:51 +0200

    i

--- unit-tests/boilerplate.c
+++ unit-tests/boilerplate.c
@@ -3,8 +3,23 @@
 
 int	declare_tests_and_run(int all_of, char *these[])
 {
-	T(nop,
-		assert_true(1);
+	T(test_unicode1,
+		wchar_t	vals[4] = {0xc8, 0xa9, 0x0a, 0};
+
+		printf_compare("s_test_unicode1.ref", "%ls", vals);
 	)
+
+	T(test_unicode2,
+		wchar_t	vals[5] = {0xc8,  0x0a, 0xa9, 0x0a, 0};
+
+		printf_compare("s_test_unicode2.ref", "%.3ls", vals);
+	)
+
+	T(test_unicode3,
+		wchar_t	vals[5] = {0xc8,  0x0a, 0xa9, 0x0a, 0};
+
+		printf_compare("s_test_unicode2.ref", "%.4ls", vals);
+	)
+
 	return(run_test_arr(all_of, these));
 }
