9c379c1d9d0849d140cd6d3f72d5e9c676f63a69
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-20 12:39:53 +0200

    i

--- unit-tests/s_conversions.test.c
+++ unit-tests/s_conversions.test.c
@@ -4,7 +4,9 @@
 int	declare_tests_and_run(int all_of, char *these[])
 {
 	T(test_unicode1,
-		wchar_t	vals[4] = {0xc8, 0xa9, 0x0a, 0};
+		wchar_t	*vals;
+
+		vals = (&(wchar_t[4]){0xc8, 0xa9, 0x0a, 0});
 
 		printf_compare("s_test_unicode1.ref", "%ls", vals);
 	)
