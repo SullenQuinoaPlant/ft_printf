06e61373d9878fb58bff2acc9ed73627d974e097
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-25 21:39:40 +0200

    i

--- sources/convert_n.c
+++ sources/convert_n.c
@@ -3,8 +3,10 @@
 void		convert_n(t_s_pct *chk)
 {
 	uintmax_t	const val = g_os.output_len;;
+	size_t		sz;
 	t_e_t		target;
 
 	target = get_etype(chk->specifier, chk->len_mod);
-	ft_memcpy(*(void**)chk->vaarg->p_val, &val, target);
+	sz = g_et_z[target];
+	ft_memcpy(*(void**)chk->vaarg->p_val, &val, sz);
 }
