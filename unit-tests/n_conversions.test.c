1977a857910678c0d2e58150e16d407c9a014949
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-07-25 21:22:36 +0200

    i

--- unit-tests/n_conversions.test.c
+++ unit-tests/n_conversions.test.c
@@ -4,29 +4,29 @@
 
 int	declare_tests_and_run(int all_of, char *these[])
 {
+	#define CMD "hello this is a string%n"
 	T(test1,
 		int nref;
 		int ntst;
 
-		#define CMD "hello this is a string%n"
 		printf(CMD, &nref);
 		printf(CMD, &ntst);
-		#undef CMD 
 
 		assert_int_equal(nref, ntst);
 	)
+	#undef CMD 
 
+	#define CMD "hello this is a string%n%n"
 	T(test2,
 		int	nref[2];
 		int	ntst[2];
 
-		#define CMD "hello this is a string%n%n"
 		printf(CMD, &nref[0], &nref[1]);
 		printf(CMD, &ntst[0], &ntst[1]);
-		#undef CMD 
 
 		assert_memory_equal(nref, ntst, sizeof(nref));
 	)
+	#undef CMD 
 
 	return(run_test_arr(all_of, these));
 }
