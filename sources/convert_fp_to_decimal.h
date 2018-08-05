f5a7914eeabf06008210c61ccea2c0802bc46eb0
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-08-05 04:07:34 +0200

    i

--- sources/convert_fp_to_decimal.h
+++ sources/convert_fp_to_decimal.h
@@ -1,9 +1,9 @@
 #ifndef CONVERT_FP_TO_DECIMAL_H
 # define CONVERT_FP_TO_DECIMAL_H
 
+# include "floating_point.h"
 # include "libmystupidmath.h"
 # include "libmyfloatingpoint.h"
-# include "floating_point.h"
 
 typedef struct	s_power_of_ten {
 	int			pow10;
