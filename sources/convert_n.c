9ca1302b323e3cbfa6cf03d228d219880d363e9c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-09-21 21:31:27 +0200

    i

--- sources/convert_n.c
+++ sources/convert_n.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   convert_n.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/21 21:12:38 by nmauvari          #+#    #+#             */
+/*   Updated: 2018/09/21 21:31:13 by nmauvari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "inner.h"
 
 static t_e_t
@@ -9,15 +21,26 @@ static t_e_t
 		e_longlong,
 		e_int,
 		e_intmax_t,
-		e_size_t,
+		e_ssize_t,
 		e_ptrdiff_t
 };
 
-void		convert_n(t_s_pct *chk)
+void					convert_n(
+	t_s_pct *chk)
 {
-	uintmax_t	const val = g_os.output_len;;
+	intmax_t	val;
 	size_t		sz;
 
 	sz = g_et_sz[g_target_et[chk->len_mod]];
+	if (g_os.output_len >= (uintmax_t)1 << (sz * CHAR_BIT - 1))
+		val = -1;
+	else
+		val = g_os.output_len;
 	ft_memcpy(*(void**)chk->vaarg->p_val, &val, sz);
+}
+
+void					convert_big_n(
+	t_s_pct *chk)
+{
+	**(size_t**)chk->vaarg->p_val = g_os.output_len;
 }
--- sources/etypes_get_etype.c
+++ sources/etypes_get_etype.c
@@ -60,7 +60,7 @@ static t_e_t const
 	//	for which the value stored is actually a pointer to the type specified by
 	//	the length modifier as follows (where defaults are set to int* see doc):
 	//		{e_int, e_char, e_short, e_long, e_longlong, e_int,
-	//			e_intmax_t, e_size_t, e_ptrdiff_t},
+	//			e_intmax_t, e_ssize_t, e_ptrdiff_t},
 	{e_voidptr, e_voidptr, e_voidptr, e_voidptr, e_voidptr, e_voidptr, e_voidptr,
 		e_voidptr, e_voidptr},
 	//e_bign
