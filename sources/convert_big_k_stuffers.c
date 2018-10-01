43f6150150a1df3595f97735218316e6015dee9c
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-01 23:29:29 +0200

    i

---
+++ sources/convert_big_k_stuffers.c
@@ -0,0 +1,53 @@
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+
+	
+}
+
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+	t_s_so *const	syl = stf->syls + pos;
+
+}
+
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+	t_s_so *const	syl = stf->syls + pos;
+
+}
+
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+	t_s_so *const	syl = stf->syls + pos;
+
+}
+
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+	t_s_so *const	syl = stf->syls + pos;
+
+}
+
+void							ck_hour(
+	int pos,
+	void *p)
+{
+	t_s_kcs *const	stf = (t_s_kcs*)p;
+	t_s_so *const	syl = stf->syls + pos;
+
+}
--- sources/convert_k.c
+++ sources/convert_k.c
@@ -6,49 +6,11 @@
 /*   By: nmauvari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/21 23:44:32 by nmauvari          #+#    #+#             */
-/*   Updated: 2018/10/01 23:15:41 by nmauvari         ###   ########.fr       */
+/*   Updated: 2018/10/01 23:24:21 by nmauvari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static char						*g_days[7] =
-{
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-}
-
-static char						*g_months[12] =
-{
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-};
-
-static t_stuffer				g_fstr[K_SYLS + 1] =
-{
-	dummy_stuffer,
-	dummy_stuffer,
-	ck_hour,
-	ck_hour_char,
-	ck_minutes,
-	ck_minutes_char,
-	ck_seconds_char,
-	dummy_stuffer,
-	0
-}
+#inlude "inner.h"
 
 #define HM_SEP 2
 #define MS_SEP 5
@@ -103,10 +65,15 @@ void							write_time(
 	}
 }
 
+/*
+**3 * 2 digits + 3 characters:
+*/
+#define K_BUFF_SIZE 9
+
 void							convert_k(
 	t_s_pct *chk)
 {
-	char	buf[9];
+	char	buf[K_BUFF_SIZE];
 	size_t	lens[e_oi_sz];
 
 	lens[e_prefix] = 0;
