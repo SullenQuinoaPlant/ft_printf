bbac10ed9ce6e58947d19781ec66ec6b0478d9c1
SullenQuinoaPlant <noe.42.2017@gmail.com>
2018-10-03 22:28:25 +0200

    i

--- unit-tests/s_conversions.test.c
+++ unit-tests/s_conversions.test.c
@@ -49,10 +49,13 @@ int	declare_tests_and_run(int all_of, char *these[])
 	)
 			
 	T(non_printable_0_Lstring,
+		wchar_t	*str;
+
+		str = ((wchar_t[32]){
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 0});
+
 		printf_compare("s_non_printable_0_Lstring.ref",
-			"this is a string with all the non printables: %#S\n",
-			L"éloïse\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
-			"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F");
+			"this is a string with all the non printables: %#S\n", str);
 	)
 			
 	return(run_test_arr(all_of, these));
